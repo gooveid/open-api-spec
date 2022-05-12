27f9feb731e236b866d4da2700633cbbc0ef22f1
Handi Deyana <handi.deyana@mncgroup.com>
2022-05-12 13:48:37 +0700

    Init Spec

---
+++ generated-site-typescript/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthApi';
+export * from './BillingApi';
+export * from './CallbackApi';
+export * from './KeysApi';
+export * from './OtpApi';
+export * from './RequestsApi';
+export * from './TeamApi';
+export * from './UserApi';
---
+++ generated-typescript/models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './OTPModel';
+export * from './OTPRequest';
+export * from './OTPResponse';
+export * from './OtpValidated';
+export * from './ValidateRequest';
